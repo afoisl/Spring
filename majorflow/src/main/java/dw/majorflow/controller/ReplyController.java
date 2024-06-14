6358e1fb587dd2e326b2eaa4876ac95a141c4363
Geonho2414 <160198328+afoisl@users.noreply.github.com>
2024-06-14 12:29:17 +0900

    2024.06.14 am 12.17

--- majorflow/src/main/java/dw/majorflow/controller/ReplyController.java
+++ majorflow/src/main/java/dw/majorflow/controller/ReplyController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/reply")
 public class ReplyController {
+    @Autowired
     private ReplyService replyService;
 
     public ReplyController(ReplyService replyService) {
@@ -20,8 +24,14 @@ public class ReplyController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<String> saveReply(@RequestBody Reply reply) {
+    public ResponseEntity<Reply> saveReply(@RequestBody Reply reply) {
         return new ResponseEntity<>(replyService.saveReply(reply),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Reply>> getAllReply() {
+        return new ResponseEntity<>(replyService.getAllReply(),
                 HttpStatus.OK);
     }
 
--- majorflow/src/main/java/dw/majorflow/service/ReplyService.java
+++ majorflow/src/main/java/dw/majorflow/service/ReplyService.java
@@ -1,11 +1,12 @@
 package dw.majorflow.service;
 
 import dw.majorflow.model.Reply;
-import dw.majorflow.model.User;
 import dw.majorflow.repository.ReplyRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -14,7 +15,11 @@ public class ReplyService {
     @Autowired
     ReplyRepository replyRepository;
 
-    public String saveReply(Reply reply) {
-        return reply.getReplyText();
+    public Reply saveReply(Reply reply) {
+        return replyRepository.save(reply);
+    }
+
+    public List<Reply> getAllReply(){
+        return replyRepository.findAll();
     }
 }
