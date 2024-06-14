1bda7f3fc9caba08bb46a59cc41c31c13ee8e3e4
afoisl <160198328+afoisl@users.noreply.github.com>
2024-06-14 12:08:05 +0900

    Update 프론트

--- majorflow/src/main/resources/static/js/customer.js
+++ majorflow/src/main/resources/static/js/customer.js
@@ -1,26 +1,48 @@
-document.querySelector(".noticeBox4").addEventListener("click", () => {
-  document.querySelector(".noticeWriteBox").classList.remove("hidden");
-  document.querySelector(".noticeBox").classList.add("hidden");
-  document.querySelector(".noticePopupBox").classList.add("hidden");
-  document.querySelector(".inquiryBox").classList.add("hidden");
+const btns = document.querySelectorAll(".noticeBox3-2");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const noticeBox2Item = btn.parentNode;
+    const isActive = noticeBox2Item.classList.contains("active");
+
+    removeActiveClasses();
+
+    if (!isActive) {
+        noticeBox2Item.classList.add("active");
+    }
+  });
 });
 
-document.querySelector(".noticeClick").addEventListener("click", () => {
-  document.querySelector(".noticePopupBox").classList.remove("hidden");
-  document.querySelector(".noticeBox").classList.add("hidden");
-  document.querySelector(".inquiryBox").classList.add("hidden");
-});
+function removeActiveClasses() {
+  btns.forEach((btn) => {
+    btn.parentNode.classList.remove("active");
+  });
+}
 
-document.querySelector(".noticeBtn").addEventListener("click", () => {
-  document.querySelector(".noticeBox").classList.remove("hidden");
-  document.querySelector(".noticeWriteBox").classList.add("hidden");
-  document.querySelector(".noticePopupBox").classList.add("hidden");
-  document.querySelector(".inquiryBox").classList.add("hidden");
-});
+document.querySelector(".noticeBox6").addEventListener("click", () => {
+    document.querySelector(".noticeWriteBox").classList.remove("hidden");
+    document.querySelector(".noticeBox").classList.add("hidden");
+    document.querySelector(".customerInquiryBox").classList.add("hidden");
+    document.querySelector(".inquiryBox1-1").classList.add("hidden");
+  });
 
-document.querySelector(".customerInquiryBtn").addEventListener("click", () => {
-  document.querySelector(".inquiryBox").classList.remove("hidden");
-  document.querySelector(".noticeBox").classList.add("hidden");
-  document.querySelector(".noticePopupBox").classList.add("hidden");
-  document.querySelector(".noticeWriteBox").classList.add("hidden");
-});
+  document.querySelector(".noticeBtn").addEventListener("click", () => {
+    document.querySelector(".noticeBox").classList.remove("hidden");
+    document.querySelector(".noticeWriteBox").classList.add("hidden");
+    document.querySelector(".customerInquiryBox").classList.add("hidden");
+    document.querySelector(".inquiryBox1-1").classList.add("hidden");
+  });
+
+  document.querySelector(".customerInquiryBtn").addEventListener("click", () => {
+    document.querySelector(".customerInquiryBox").classList.remove("hidden");
+    document.querySelector(".noticeWriteBox").classList.add("hidden");
+    document.querySelector(".noticeBox").classList.add("hidden");
+    document.querySelector(".inquiryBox1-1").classList.add("hidden");
+  });
+
+  document.querySelector(".customerInquiryBox6").addEventListener("click", () => {
+    document.querySelector(".inquiryBox1-1").classList.remove("hidden");
+    document.querySelector(".noticeWriteBox").classList.add("hidden");
+    document.querySelector(".noticeBox").classList.add("hidden");
+    document.querySelector(".customerInquiryBox").classList.add("hidden");
+  });
