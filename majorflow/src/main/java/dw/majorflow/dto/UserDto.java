b47ea8b1eb6ecd858e38b864ca440d53b972118b
afoisl <160198328+afoisl@users.noreply.github.com>
2024-06-17 15:13:51 +0900

    안녕

--- majorflow/src/main/java/dw/majorflow/dto/UserDto.java
+++ majorflow/src/main/java/dw/majorflow/dto/UserDto.java
@@ -1,10 +1,14 @@
 package dw.majorflow.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class UserDto {
