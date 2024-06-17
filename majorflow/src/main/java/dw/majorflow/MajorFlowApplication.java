8732b83b131dfbfffcebd5266406f5a70bec2de4
Geonho2414 <160198328+afoisl@users.noreply.github.com>
2024-06-17 15:19:18 +0900

    Update MajorFlowApplication.java

--- majorflow/src/main/java/dw/majorflow/MajorFlowApplication.java
+++ majorflow/src/main/java/dw/majorflow/MajorFlowApplication.java
@@ -9,5 +9,5 @@ public class MajorFlowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MajorFlowApplication.class, args);
 	}
-
+// 우리의 첫번째 프로젝트 입니다. 함께 연합하여 힘을 내 봅시다.
 }
