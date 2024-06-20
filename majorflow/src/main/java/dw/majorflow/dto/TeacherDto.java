016b6d290408af7245a88861016cfa609f893abb
Geonho2414 <160198328+afoisl@users.noreply.github.com>
2024-06-20 09:42:50 +0900

    Create TeacherDto.java

---
+++ majorflow/src/main/java/dw/majorflow/dto/TeacherDto.java
@@ -0,0 +1,17 @@
+package dw.majorflow.dto;
+
+import dw.majorflow.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TeacherDto {
+    private Long teacherId;
+    private User user;
+    private String teacherName;
+}
