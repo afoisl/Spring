4443df2b6f08c8a4049949646deff201d4655f98
Geonho2414 <160198328+afoisl@users.noreply.github.com>
2024-06-20 09:35:47 +0900

    2024.06.20 건호

--- majorflow/src/main/java/dw/majorflow/controller/TeacherController.java
+++ majorflow/src/main/java/dw/majorflow/controller/TeacherController.java
@@ -1,46 +1,25 @@
 package dw.majorflow.controller;
 
-<<<<<<< HEAD
-=======
 import dw.majorflow.dto.TeacherDto;
->>>>>>> 134ed2bf6f4f3bb74e16b22a16cae33dae0f70ce
 import dw.majorflow.model.Teacher;
 import dw.majorflow.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-=======
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> 134ed2bf6f4f3bb74e16b22a16cae33dae0f70ce
-
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
-<<<<<<< HEAD
-    private final TeacherService teacherService;
-
-    @Autowired
-    public TeacherController(TeacherService teacherService) {
-        this.teacherService = teacherService;
-    }
-    @GetMapping("/{teacherId}")
-    public ResponseEntity<Teacher> getTeacherById(@PathVariable Long teacherId) {
-        Teacher teacher = teacherService.getTeacherById(teacherId);
-        return new ResponseEntity<>(teacher, HttpStatus.OK);
-=======
     @Autowired
     TeacherService teacherService;
 
     @GetMapping("/get/{id}")
     public TeacherDto getTeacherById(@PathVariable Long id) {
         return teacherService.getTeacherById(id);
->>>>>>> 134ed2bf6f4f3bb74e16b22a16cae33dae0f70ce
     }
 }
--- majorflow/src/main/java/dw/majorflow/service/TeacherService.java
+++ majorflow/src/main/java/dw/majorflow/service/TeacherService.java
@@ -1,13 +1,10 @@
 package dw.majorflow.service;
 
-<<<<<<< HEAD
 import dw.majorflow.model.Teacher;
 import dw.majorflow.model.User;
-=======
 import dw.majorflow.dto.TeacherDto;
 import dw.majorflow.exception.ResourceNotFoundException;
 import dw.majorflow.model.Teacher;
->>>>>>> 134ed2bf6f4f3bb74e16b22a16cae33dae0f70ce
 import dw.majorflow.repository.TeacherRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,23 +15,7 @@ import java.util.Optional;
 @Service
 @Transactional
 public class TeacherService {
-<<<<<<< HEAD
-    private final TeacherRepository teacherRepository;
 
-    @Autowired
-    public TeacherService(TeacherRepository teacherRepository) {
-        this.teacherRepository = teacherRepository;
-    }
-
-    public Teacher getTeacherById(Long teacherId) {
-        Optional<Teacher> teacherOptional = teacherRepository.findByTeacherId(teacherId);
-        if (teacherOptional.isPresent()) {
-            return teacherOptional.get();
-        } else {
-            throw new RuntimeException("Teacher not found with id: " + teacherId);
-        }
-    }
-=======
     @Autowired
     TeacherRepository teacherRepository;
 
@@ -54,5 +35,4 @@ public class TeacherService {
         dto.setTeacherName(teacher.getTeacherName());
         return dto;
     }
->>>>>>> 134ed2bf6f4f3bb74e16b22a16cae33dae0f70ce
 }
