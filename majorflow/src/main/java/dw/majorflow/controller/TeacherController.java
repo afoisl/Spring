4280c00b05b8536d18b09553a361a486286bd16b
Geonho2414 <160198328+afoisl@users.noreply.github.com>
2024-06-24 10:27:00 +0900

    모든 선생님

--- majorflow/src/main/java/dw/majorflow/controller/TeacherController.java
+++ majorflow/src/main/java/dw/majorflow/controller/TeacherController.java
@@ -2,6 +2,7 @@ package dw.majorflow.controller;
 
 import dw.majorflow.dto.TeacherDto;
 import dw.majorflow.model.Teacher;
+import dw.majorflow.model.User;
 import dw.majorflow.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,12 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Teacher>> getAllTeacher() {
+        return new ResponseEntity<>(teacherService.getAllTeacher(), HttpStatus.OK);
+    }
 
     @GetMapping("/get/{id}")
     public TeacherDto getTeacherById(@PathVariable Long id) {
--- majorflow/src/main/java/dw/majorflow/service/TeacherService.java
+++ majorflow/src/main/java/dw/majorflow/service/TeacherService.java
@@ -10,6 +10,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,11 @@ public class TeacherService {
 
     @Autowired
     TeacherRepository teacherRepository;
+
+    // 모든 강사 다 받아오는 코드
+    public List<Teacher> getAllTeacher() {
+        return teacherRepository.findAll();
+    }
 
     // 강사Id를 받아와서 강사Id와 강사이름을 출력하는 코드
     public TeacherDto getTeacherById(Long id) {
