23090a58e66b9edbc3684e494bb8fc22edd59794
afoisl <160198328+afoisl@users.noreply.github.com>
2024-06-27 10:50:49 +0900

    맛보기 강좌 정렬 / 강좌 텍스트 수정 / 강사 사진 추가

--- majorflow/target/classes/static/js/signup.js
+++ majorflow/target/classes/static/js/signup.js
@@ -153,7 +153,7 @@ function register() {
 
   // 비밀번호 길이 검사
   if (password.length < 6 || password.length > 20) {
-    alert("비밀번호는 6~20자로 되어야 합니다.");
+    action_popup.alert("비밀번호는 6~20자가 되어야합니다.");
     document.getElementById("password").focus();
     return;
   }
@@ -297,3 +297,60 @@ function getRadioBtn(name) {
   let values = arrayElements.filter((e) => e.checked).map((e) => e.value);
   return values;
 }
+
+// 모달창 테스트
+
+var action_popup = {
+  timer: 500,
+  confirm: function (txt, callback) {
+    if (txt == null || txt.trim() == "") {
+      console.warn("confirm message is empty.");
+      return;
+    } else if (callback == null || typeof callback != "function") {
+      console.warn("callback is null or not function.");
+      return;
+    } else {
+      $(".type-confirm .btn_ok").on("click", function () {
+        $(this).unbind("click");
+        callback(true);
+        action_popup.close(this);
+      });
+      this.open("type-confirm", txt);
+    }
+  },
+
+  alert: function (txt) {
+    if (txt == null || txt.trim() == "") {
+      console.warn("confirm message is empty.");
+      return;
+    } else {
+      this.open("type-alert", txt);
+    }
+  },
+
+  open: function (type, txt) {
+    var popup = $("." + type);
+    popup.find(".menu_msg").text(txt);
+    $("body").append("<div class='dimLayer'></div>");
+    $(".dimLayer").css("height", $(document).height()).attr("target", type);
+    popup.fadeIn(this.timer);
+  },
+
+  close: function (target) {
+    var modal = $(target).closest(".modal-section");
+    var dimLayer;
+    if (modal.hasClass("type-confirm")) {
+      dimLayer = $(".dimLayer[target=type-confirm]");
+      $(".type-confirm .btn_ok").unbind("click");
+    } else if (modal.hasClass("type-alert")) {
+      dimLayer = $(".dimLayer[target=type-alert]");
+    } else {
+      console.warn("close unknown target.");
+      return;
+    }
+    modal.fadeOut(this.timer);
+    setTimeout(function () {
+      dimLayer != null ? dimLayer.remove() : "";
+    }, this.timer);
+  },
+};
